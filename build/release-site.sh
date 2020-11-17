25f8cfff0735506287b01bf795cbd43b22cdec53
张腾跃 <tengyuezhang@91jkys.com>
2020-11-17 19:50:30 +0800

    feat(ci): ci

--- build/release-site.sh
+++ build/release-site.sh
@@ -3,8 +3,8 @@ rm -rf docs/dist
 
 npx cross-env NODE_ENV=production webpack --config build/webpack.doc.prd.js
 
-superman-cdn /vant-weapp ./docs/dist/*.js
+# superman-cdn /vant-weapp ./docs/dist/*.js
 
-rm -rf docs/dist/*.js
+# rm -rf docs/dist/*.js
 
-gh-pages -d docs/dist --add
+# gh-pages -d docs/dist --add
--- build/webpack.doc.prd.js
+++ build/webpack.doc.prd.js
@@ -7,7 +7,6 @@ module.exports = Object.assign(config, {
   mode: 'production',
   output: {
     path: path.join(__dirname, '../docs/dist'),
-    publicPath: 'https://b.yzcdn.cn/vant-weapp/',
     filename: '[name].[hash:8].js',
     chunkFilename: 'async_[name].[chunkhash:8].js',
   },
