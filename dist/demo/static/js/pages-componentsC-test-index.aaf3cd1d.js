7aa0782d1358182da87bbd9f2d32da7eba9648f2
qiuwww <1014913222@qq.com>
2021-11-03 17:03:24 +0800

    feat: 本地打包，发布代码

---
+++ dist/demo/static/js/pages-componentsC-test-index.aaf3cd1d.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["pages-componentsC-test-index"],{"8ff9":function(e,n,t){"use strict";var s;t.r(n);var c,u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("v-uni-view")},a=[],i=t("f0c5"),l={},o=Object(i["a"])(l,u,a,!1,null,null,null,!1,s,c);n["default"]=o.exports}}]);
